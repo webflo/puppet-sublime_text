da3aab12c3191cec66fdb5a61ca44e9b4b0f3e20
Steve Rude <srude@riotgames.com>
2013-06-06 13:13:31 -0700

    update spec

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,7 +4,7 @@
 #
 #     include sublime_text
 class sublime_text {
-  package { 'SublimeText':
+  package { 'Sublime Text':
     provider => 'appdmg',
     source   => 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203033.dmg';
   }
@@ -13,6 +13,6 @@ class sublime_text {
     ensure  => link,
     target  => '/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl',
     mode    => '0755',
-    require => Package['SublimeText'],
+    require => Package['Sublime Text'],
   }
 }
--- spec/classes/sublime_text_spec.rb
+++ spec/classes/sublime_text_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'sublime_text' do
   it do
-    should contain_package('SublimeText').with({
+    should contain_package('Sublime Text').with({
       :provider => 'appdmg',
       :source   => 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203033.dmg'
     })
